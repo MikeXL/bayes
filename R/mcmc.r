c14cb7ebc7f441eefef4fb557abccd4d4285db68
Mike <mike@mscanada.net>
2015-11-29 00:59:55 -0800

    too bad, hamiltonian

--- R/mcmc.r
+++ R/mcmc.r
@@ -12,7 +12,20 @@ simple.proposal.fun <- function(theta){
 }
 
 hamiltonian.proposal.fun <- function(theta){
-  return(theta+rnorm(1))
+  #forget it.. it is a good theory
+  # H(q, p) = U(q) + K(p) .. the potential and kinetic energy
+  # simulate data by the way of the physical system moves than a random walk
+  # faster convergence
+  # more parameters to worry: epsilon, M, t, L
+  # not to mention the change of proposal..
+  #   U(q) = -log[pi(q) * L(q|D)]
+  # hmc proposal and actual simulation is tight up
+  #  condition:
+  #     runif(1) < exp(-U(q*)+U(q)-K(p*)+K(p))
+  #
+  # good read, I'll leave the work for others :)
+  #
+  return(theta+rnorm(length(theta)))
 }
 
 mcmc <- function(fun, theta.init, nmc, nbi, hmc=FALSE, ...) {
