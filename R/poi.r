e8f7183cbccb85328a8d0b5f75defe6c6d537e60
Mike <mike@mscanada.net>
2015-12-21 13:24:26 -0800

    poisson test

--- R/poi.r
+++ R/poi.r
@@ -1,11 +1,20 @@
 poisson.fun <- function(theta, x, y) {
+  # parameters
+  lambda1 <- theta[1]
+  lambda2 <- theta[2]
 
+  # priors
+  priors <- dgamma(lambda1, .5, 1e-5, log=T) + dgamma(lambda2, .5, 1e-5, log=T)
 
+  # likelihood
+  ll <- dpois(x, lambda1, log=T) + dpois(y, lambda2, log=T)
+
+  return (ll + priors)
 }
 
 
-bayes.poisson.test <- function(x, y, nmc=20000, nbi=20000)
-  theta.init <- ?
+bayes.poisson.test <- function(x, y, nmc=20000, nbi=20000) {
+  theta.init <- c(rgamma(1, .5, 1e-5), rgamma(1, .5, 1e-5))
   out <- bayes::mcmc(poisson.fun, theta.init, nmc, nbi, x=x, y=y)
   colnames(out) <- c("lambda1", "lambda2")
   return(out)
