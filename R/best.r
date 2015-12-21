f6534b6e60a46da47dade4be4ea9661012817b58
Mike <mike@mscanada.net>
2015-12-21 12:57:53 -0800

    add one group t.test

--- R/best.r
+++ R/best.r
@@ -1,3 +1,4 @@
+# two samples (group) t.test
 ttest.fun <- function(theta, x, y) {
   # parameters
 	mu1 <- theta[1]
@@ -27,8 +28,40 @@ ttest.fun <- function(theta, x, y) {
 	ifelse(is.na(ll) | is.nan(ll), -Inf, ll)                            # deal with the missing values
 }
 
-bayes.t.test <- function(x, y, nmc=20000, nbi=20000) {
-  out <- bayes::mcmc(ttest.fun, c(mean(x), sd(x), mean(y), sd(y), 5), nmc, nbi, x=x, y=y)
-  colnames(out) <- c("mu1", "sd1", "mu2", "sd2", "nu")
+# one sample (group) t.test
+# or two dependent (paired) t.test
+ttest.1g.fun <- function(theta, x) {
+  # parameters
+	mu <- theta[1]
+	sd <- theta[2]
+	nu <- theta[3]
+
+  # no negative standard deviations, right?!
+	if(sd <=0) { return(-Inf) }
+
+  # priors
+  log.prior <- dnorm(mu, sd = sd*1e3,  log=T) +                       # mu ~ N(0, sd*1e3)
+               dunif(sd, min= sd*1e-3, max=sd*1e3, log=T) +           # sd1 ~ unif(sd*1e-3, sd*1e3)
+  				     dexp((nu-1), 1/29, log=T)                              # nu  ~ exp(1/29) + 1
+
+  # likelihood
+	log.like <- sum(dt({x-mu}/sd, df=nu, log=T) - log(sd))              # x ~ T(mu, sd, nu) ~ t({x-mu}/sd, nu)/sd
+
+  # posterior
+	ll <- log.like + log.prior
+
+	ifelse(is.na(ll) | is.nan(ll), -Inf, ll)                            # deal with the missing values
+}
+
+# accuracy goes 1/sqrt(nmc),
+# therefore may appropriate round(out, int(sqrt(nmc) % 10))
+bayes.t.test <- function(x, y=NULL, nmc=20000, nbi=20000) {
+  if(is.null(y)) {                                                   # one group
+    out <- bayes::mcmc(ttest.fun, c(mean(x), sd(x), 5), nmc, nbi, x=x)
+    colnames(out) <- c("mu", "sd", "nu")
+  } else {                                                           # two groups
+    out <- bayes::mcmc(ttest.fun, c(mean(x), sd(x), mean(y), sd(y), 5), nmc, nbi, x=x, y=y)
+    colnames(out) <- c("mu1", "sd1", "mu2", "sd2", "nu")
+  }
   return(out)
 }
