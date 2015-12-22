341c31f1db3d81c4a5b9b25e53d9fe7476575570
Mike <mike@mscanada.net>
2015-12-21 19:45:05 -0800

    deal with missing value in posterior function

--- R/poi.r
+++ R/poi.r
@@ -4,17 +4,19 @@ poisson.fun <- function(theta, x, y) {
   lambda2 <- theta[2]
 
   # priors
-  priors <- dgamma(lambda1, .5, 1e-5, log=T) + dgamma(lambda2, .5, 1e-5, log=T)
+  log.priors <- dgamma(lambda1, .5, 1e-5, log=T) + dgamma(lambda2, .5, 1e-5, log=T)
 
   # likelihood
-  ll <- dpois(x, lambda1, log=T) + dpois(y, lambda2, log=T)
+  log.like <- dpois(x, lambda1, log=T) + dpois(y, lambda2, log=T)
 
-  return (ll + priors)
+  ll <- log.priors + log.like
+  
+  ifelse(is.na(ll) | is.nan(ll), -Inf, ll)
 }
 
 
 bayes.poisson.test <- function(x, y, nmc=20000, nbi=20000) {
-  theta.init <- c(rgamma(1, .5, 1e-5), rgamma(1, .5, 1e-5))
+  theta.init <- c(mean(x), mean(y))
   out <- bayes::mcmc(poisson.fun, theta.init, nmc, nbi, x=x, y=y)
   colnames(out) <- c("lambda1", "lambda2")
   return(out)
