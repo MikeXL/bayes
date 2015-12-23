ef935b7cd98f4fe20107db1ea66d71d4f03db450
Mike <mike@mscanada.net>
2015-12-22 20:45:08 -0800

    one group poisson mean (lambda) estimation

--- R/poi.r
+++ R/poi.r
@@ -7,17 +7,37 @@ poisson.fun <- function(theta, x, y) {
   log.priors <- dgamma(lambda1, .5, 1e-5, log=T) + dgamma(lambda2, .5, 1e-5, log=T)
 
   # likelihood
-  log.like <- dpois(x, lambda1, log=T) + dpois(y, lambda2, log=T)
+  log.like <- sum(dpois(x, lambda1, log=T)) + sum(dpois(y, lambda2, log=T))
 
   ll <- log.priors + log.like
-  
+
   ifelse(is.na(ll) | is.nan(ll), -Inf, ll)
 }
 
+poisson.1g.fun <- function(theta, x) {
+  # parameters
+  lambda <- theta[1]
 
-bayes.poisson.test <- function(x, y, nmc=20000, nbi=20000) {
-  theta.init <- c(mean(x), mean(y))
-  out <- bayes::mcmc(poisson.fun, theta.init, nmc, nbi, x=x, y=y)
-  colnames(out) <- c("lambda1", "lambda2")
+  # priors
+  log.priors <- dgamma(lambda, .5, 1e-5, log=T)
+
+  # likelihood
+  log.like <- sum(dpois(x, lambda, log=T))
+
+  ll <- log.priors + log.like
+
+  ifelse(is.na(ll) | is.nan(ll), -Inf, ll)
+}
+
+bayes.poisson.test <- function(x, y=NULL, nmc=20000, nbi=20000) {
+  if(is.null(y)) {
+    theta.init <- (sum(x)+.5)/length(x)
+    out <- bayes::mcmc(poisson.1g.fun, theta.init, nmc, nbi, x=x)
+    colnames(out) <- c("lambda")
+  } else {
+    theta.init <- c((sum(x)+.5)/length(x), (sum(y)+.5)/length(y))
+    out <- bayes::mcmc(poisson.fun, theta.init, nmc, nbi, x=x, y=y)
+    colnames(out) <- c("lambda1", "lambda2")
+  }
   return(out)
 }
