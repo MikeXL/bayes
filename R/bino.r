2524815867ee98343978ee9878bf7f02f99a5960
Mike <mike@mscanada.net>
2015-12-23 23:18:57 -0800

    two groups estimation

--- R/bino.r
+++ R/bino.r
@@ -2,24 +2,25 @@ binomial.fun <- function(theta, x, y) {
   # parameters
   # theta1, theta2
 
-  N.Success <- x
-  N.Trial   <- y
-
   # flat priors
-  log.priors <- sum(dbeta(theta, 1, 1, log=T))
+  #log.priors <- sum(dbeta(theta[1], 1, 1, log=T)) +
+  #              sum(dbeta(theta[2], 1, 1, log=T))
+
   # Jeffrey's priors
-  # log.priors <- dbeta(theta, .5, .5, log=T)
+  log.priors <- sum(dbeta(theta[1], .5, .5, log=T)) +
+                sum(dbeta(theta[2], .5, .5, log=T))
 
   # likelihood
-  log.like <- sum(dbinom(N.Success, N.Trial, theta, log=T))
+  log.like <- sum(dbinom(x[, 1], x[, 2], theta[1], log=T)) +
+              sum(dbinom(y[, 1], y[, 2], theta[2], log=T))
 
   ll <- log.priors + log.like
 
   ifelse(is.na(ll) | is.nan(ll), -Inf, ll)
 }
 
-bayes.binomial.test <- function(x, n, nmc=20000, nbi=20000) {
-  theta.init <- (sum(x)+1) / (sum(n)+2)
-  out <- bayes::mcmc(binomial.fun, theta.init, nmc, nbi, x=x, y=n)
+bayes.binomial.test <- function(x, y, nmc=20000, nbi=20000) {
+  theta.init <- c((sum(x[, 1])+1) / (sum(x[, 2])+2), (sum(y[, 1])+1) / (sum(y[, 2])+2))
+  out <- bayes::mcmc(binomial.fun, theta.init, nmc, nbi, x=x, y=y)
   return(out)
 }
