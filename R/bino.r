4f8af697bd45cbdd936acba3034380022d8bd6c7
Mike <mike@mscanada.net>
2015-12-23 23:55:44 -0800

    binomial (exact).. arghhh. forgot the exact

--- R/bino.r
+++ R/bino.r
@@ -1,26 +1,25 @@
-binomial.fun <- function(theta, x, y) {
+binomial.fun <- function(theta, x) {
+
   # parameters
-  # theta1, theta2
+  # x[, 1] ... number of success
+  # x[, 2] ... number of trials
 
   # flat priors
-  #log.priors <- sum(dbeta(theta[1], 1, 1, log=T)) +
-  #              sum(dbeta(theta[2], 1, 1, log=T))
+  #log.priors <- sum(dbeta(theta, 1, 1, log=T))
 
   # Jeffrey's priors
-  log.priors <- sum(dbeta(theta[1], .5, .5, log=T)) +
-                sum(dbeta(theta[2], .5, .5, log=T))
+  log.priors <- sum(dbeta(theta, .5, .5, log=T))
 
   # likelihood
-  log.like <- sum(dbinom(x[, 1], x[, 2], theta[1], log=T)) +
-              sum(dbinom(y[, 1], y[, 2], theta[2], log=T))
+  log.like <- sum(dbinom(x[, 1], x[, 2], theta, log=T))
 
   ll <- log.priors + log.like
 
   ifelse(is.na(ll) | is.nan(ll), -Inf, ll)
 }
 
-bayes.binomial.test <- function(x, y, nmc=20000, nbi=20000) {
-  theta.init <- c((sum(x[, 1])+1) / (sum(x[, 2])+2), (sum(y[, 1])+1) / (sum(y[, 2])+2))
-  out <- bayes::mcmc(binomial.fun, theta.init, nmc, nbi, x=x, y=y)
+bayes.binomial.test <- function(x, nmc=20000, nbi=20000) {
+  theta.init <- (sum(x[, 1])+1) / (sum(x[, 2])+2)
+  out <- bayes::mcmc(binomial.fun, theta.init, nmc, nbi, x=x)
   return(out)
 }
