9060e42685cf7f644a4ed2f6e0ef0a44de86baaf
Mike <mike@mscanada.net>
2015-12-26 10:59:59 -0800

    flexible prior setting

--- R/bino.r
+++ R/bino.r
@@ -1,14 +1,13 @@
-binomial.fun <- function(theta, x) {
+binomial.fun <- function(theta, x, prior) {
 
   # parameters
   # x[, 1] ... number of success
   # x[, 2] ... number of trials
 
-  # flat priors
-  #log.priors <- sum(dbeta(theta, 1, 1, log=T))
-
-  # Jeffrey's priors
-  log.priors <- sum(dbeta(theta, .5, .5, log=T))
+  # priors
+  #  flat beta(1, 1)
+  #  jeffery beta(.5, .5)
+  log.priors <- sum(dbeta(theta, prior[1], prior[2], log=T))
 
   # likelihood
   log.like <- sum(dbinom(x[, 1], x[, 2], theta, log=T))
@@ -18,8 +17,8 @@ binomial.fun <- function(theta, x) {
   ifelse(is.na(ll) | is.nan(ll), -Inf, ll)
 }
 
-bayes.binomial.test <- function(x, nmc=20000, nbi=20000) {
+bayes.binomial.test <- function(x, prior=c(1, 1), nmc=20000, nbi=20000) {
   theta.init <- (sum(x[, 1])+1) / (sum(x[, 2])+2)
-  out <- bayes::mcmc(binomial.fun, theta.init, nmc, nbi, x=x)
+  out <- bayes::mcmc(binomial.fun, theta.init, nmc, nbi, x=x, prior=prior)
   return(out)
 }
