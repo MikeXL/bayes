e409b76b4d0883aa9fecc1fc61d49d591425c210
Mike <MikeXL@users.noreply.github.com>
2020-07-27 14:14:14 -0700

    start ...

---
+++ src/mc.f
@@ -0,0 +1,12 @@
+C sketch 
+C        metropolis hasting monte carlo 
+C        hamiltonian monte carlo
+C not really gonna use it, consider there are 
+C MCMCpack and STAN
+C rather for fun and jot down thoughts
+C23456789
+      subroutine metrop()
+      end subroutine metrop
+      
+      subroutine ham()
+      end subroutine ham
