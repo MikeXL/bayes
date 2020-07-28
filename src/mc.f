434a051f4a7356f1112c3b5dbeb6a88a61286d44
Mike <MikeXL@users.noreply.github.com>
2020-07-27 21:18:52 -0700

    metrop ideas

--- src/mc.f
+++ src/mc.f
@@ -6,10 +6,32 @@ C MCMCpack and STAN
 C rather for fun and jot down thoughts
 C
 C   arXiv:1808.08490v2  
+C   MikeXL/bayes/R/mcmc.r
 C
+C metropolis hasting, the simplest one 
+C target the logged posterior function
+C nmc number iterations 
+C nbi burnin 
+C chain the Markov chain 
 C23456789
-      subroutine metrop()
+      subroutine metrop(target, nmc, nbi, chain)
+        call random_number(chain(1, :)) 
+        do 71 i = 2, nmc
+          x = chain(i-1, :)
+          call random_num(chain(i, :)
+
+          if rand() < exp(target(chain(i, :)) - target(x))
+          then 
+C accept
+          else
+C reject 
+            chain(i, :) = x
+          end 
+ 71     continue 
       end subroutine metrop
-      
+C
+C Hamiltonian, put out your sketchbook 
+C derivatives 
+C
       subroutine ham()
       end subroutine ham
