9379e7b393622ca6b41f643de39868298f3e69e3
Mike <MikeXL@users.noreply.github.com>
2020-07-27 20:42:39 -0700

    Update mc.f

--- src/mc.f
+++ src/mc.f
@@ -4,6 +4,9 @@ C        hamiltonian monte carlo
 C not really gonna use it, consider there are 
 C MCMCpack and STAN
 C rather for fun and jot down thoughts
+C
+C   arXiv:1808.08490v2  
+C
 C23456789
       subroutine metrop()
       end subroutine metrop
